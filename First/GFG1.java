45c2ffba22e61047c28ec00d41ebe89ab1a65efd
beeplaw <bsc19108@cab.edu.np>
2023-04-23 19:28:46 +0545

    Made some adjustments to the directory and files

--- First/GFG1.java
+++ First/GFG1.java
@@ -3,15 +3,16 @@
 package First;
  
 public class GFG1 {
- 
+
     int a;
     int b;
+
     // Creating constructor
-    GFG1(int a, int b)
+    public GFG1(int a, int b)
     {
         this.a = a;
         this.b = b;
     }
     // Function to add the members of the class
-    int add() { return this.a + this.b; }
+    public int add() { return this.a + this.b; }
 }
